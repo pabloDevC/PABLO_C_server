db2980f8331e30fb57de0f59e7faf0192fa9f382
goseungwon <knmy0925@gmail.com>
2022-09-18 10:55:33 +0900

    [Chore] 프로젝트 생성

---
+++ src/test/java/com/kusitms/pablo/PabloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kusitms.pablo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PabloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
